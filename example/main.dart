d3bf060aa64e92a0cbc8a716a4a6906f1855bd66
totemdeveloper <developer@mytotem.app>
2023-04-17 06:56:52 +0500

    custom_pdf_converter prepared

---
+++ example/main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:custom_pdf_converter/widgets.dart' as pw;
+
+Future<void> main() async {
+  final pdf = pw.Document();
+
+  pdf.addPage(
+    pw.Page(
+      build: (pw.Context context) => pw.Center(
+        child: pw.Text('Hello World!'),
+      ),
+    ),
+  );
+
+  final file = File('example.pdf');
+  await file.writeAsBytes(await pdf.save());
+}
